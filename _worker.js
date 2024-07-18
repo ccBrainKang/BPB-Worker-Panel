9125af7c684632b5200a2283cc7818ea4d5db75e
ccBrainKang <103889377+ccBrainKang@users.noreply.github.com>
2024-07-18 10:43:02 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '18f6b54a-e3cd-47a3-90ea-c0ccc9c72478';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
